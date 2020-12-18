6bad3b2c9ffecfcd3b014ff1e790404d64d262bd
foeu <fabiano.junior@gft.com>
2020-12-18 15:40:47 -0300

    Implementação de autenticação com JWT

---
+++ src/Api.Data/Implementations/UserImplementation.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Api.Data.Data;
+using Api.Data.Repository;
+using Api.Domain.Entities;
+using Api.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data.Implementations
+{
+    public class UserImplementation : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+        public UserImplementation(ApplicationDbContext database) : base(database)
+        {
+            _dataset = database.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
