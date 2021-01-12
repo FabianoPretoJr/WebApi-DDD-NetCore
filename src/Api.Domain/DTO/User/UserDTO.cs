abc7427ec9bbe5d15b1bbf812ea99ff146cc5333
foeu <fabiano.junior@gft.com>
2021-01-12 13:31:37 -0300

    Criação das class DTO

---
+++ src/Api.Domain/DTO/User/UserDTO.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.DTO.User
+{
+    public class UserDTO
+    {
+        [Required(ErrorMessage = "Nome é campo obrigatório")]
+        [StringLength(60, ErrorMessage = "Nome deve ter no máximo {1} caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Nome é campo obrigatório")]
+        [StringLength(100, ErrorMessage = "E-mail deve ter no máximo {1} caracteres")]
+        [EmailAddress(ErrorMessage = "e-mail em formato inválido")]
+        public string Email { get; set; }
+    }
+}
